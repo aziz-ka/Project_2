9cc145eb5d51a24604a0e452ed80e92f343617c7
jdwmurph <jdwmurph@gmail.com>
2014-06-11 16:14:06 -0400

    Added routes for users, user_params, and active record actions, but no redirect for show, edit, update, destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,18 +4,26 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def create
   end
 
   def update
+    @user = User.update(user_params)
   end
 
   def destroy
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 end
 
